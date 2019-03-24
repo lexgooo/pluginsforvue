8f4d7d2fdfb0be38ccb9e891940a57752a6e2998
lexgooo <beyondc@foxmail.com>
2019-03-24 23:22:20 +0800

    class逻辑修改

--- src/tree/tree.vue
+++ src/tree/tree.vue
@@ -1,131 +1,137 @@
 <script>
 export default {
-    name: 'tree',
-    props: {
-        datas: {
-            type: [Array, Object]
-        }
+  name: 'tree',
+  props: {
+    datas: {
+      type: [Array, Object]
+    }
+  },
+  render(h) {
+    const _this = this
+    if (_this.datas instanceof Array) {
+      return _this.renderEl(h, _this.datas)
+    } else {
+      return h(
+        'div',
+        {
+          class: ['main', 'item-has-children']
+        },
+        [
+          h(
+            'div',
+            {
+              class: 'item'
+            },
+            [
+              h(
+                'span',
+                {
+                  class: 'item-content'
+                },
+                [_this.datas.title]
+              )
+            ]
+          ),
+          _this.renderEl(h, _this.datas.children)
+        ]
+      )
+    }
+  },
+  methods: {
+    renderEl(h, datas) {
+      const _this = this
+      return h(
+        'ul',
+        {
+          class: 'parant'
+        },
+        [_this.renderItem(h, datas)]
+      )
     },
-    render (h) {
-        const _this = this
-        if (_this.datas instanceof Array) {
-            return _this.renderEl(h, _this.datas)
+    renderItem(h, datas) {
+      const _this = this
+      return datas.map(item => {
+        if (item.children) {
+          return h('li', { class: 'item-has-children', key: item.id }, [
+            h(
+              'div',
+              {
+                class: 'item'
+              },
+              [
+                h(
+                  'span',
+                  {
+                    class: 'item-content'
+                  },
+                  [item.title]
+                )
+              ]
+            ),
+            _this.renderEl(h, item.children)
+          ])
         } else {
-            return h(
+          return h(
+            'li',
+            {
+              key: item.id
+            },
+            [
+              h(
                 'div',
                 {
-                    'class': 'parant'
+                  class: 'item'
                 },
                 [
-                    h(
-                        'span',
-                        {
-                            'class': ['parant-item', 'item-has-children']
-                        },
-                        [_this.datas.title]
-                    ),
-                    _this.renderEl(h, _this.datas.children)
+                  h(
+                    'span',
+                    {
+                      class: 'item-content'
+                    },
+                    [item.title]
+                  )
                 ]
-            )
+              )
+            ]
+          )
         }
-    },
-    methods: {
-        renderEl(h, datas) {
-            const _this = this
-            return h(
-                'ul',
-                {
-                    'class': 'children-form'
-                },
-                [_this.renderItem(h, datas)]
-            )
-        },
-        renderItem(h, datas) {
-            const _this = this
-            return datas.map(item => {
-                if (item.children) {
-                    return h(
-                        'li',
-                        {   'class': 'item-has-children',
-                            key: item.id
-                        },
-                        [
-                            h(
-                                'div',
-                                {
-                                    'class': 'item'
-                                },
-                                [item.title]
-                            ),
-                             _this.renderEl(h, item.children)
-                        ]
-                    )
-                } else {
-                    return h(
-                        'li',
-                        {   
-                            key: item.id
-                        },
-                        [
-                            h(
-                                'div',
-                                {
-                                    'class': 'item'
-                                },
-                                [item.title]
-                            )
-                        ]
-                    )
-                }
-            })
-        }
+      })
     }
+  }
 }
 </script>
 <style lang="postcss" scoped>
+.main {
+    display: flex;
+}
 .parant {
-  display: flex;
-  align-items: center;
-  /* & .item {
-
-        } */
-}
-.item,
-.parant-item {
-  position: relative;
-  margin: 12px;
-  padding: 10px;
-  border: 1px solid #ccc;
-}
-.item {
-  display: flex;
-  align-items: center;
-  &::before {
-    content: '';
-    position: absolute;
-    left: -12px;
-    height: 1px;
-    width: 12px;
-    background-color: #ccc;
-  }
-}
-li {
-    border-left: 1px solid #ccc;
+    /* display: flex; */
 }
 .item-has-children {
     display: flex;
-  & > .item {
+    align-items: center;
+    &>.item > .item-content {
+        position: relative;
+        display: flex;
+        align-item: center;
+        &::after {
+            content: '';
+            position: absolute;
+            right: -10px;
+            width: 10px;
+            height: 1px;
+            background-color: #ccc;
+        }
+    }
+}
+.item {
     display: flex;
     align-items: center;
-    &::after {
-      content: '';
-      position: absolute;
-      right: -12px;
-      height: 1px;
-      width: 12px;
-      background-color: #ccc;
-    }
-  }
+    padding: 10px;
+}
+.item-content {
+padding: 10px;
+border: 1px solid #ccc;
 }
 </style>
 
